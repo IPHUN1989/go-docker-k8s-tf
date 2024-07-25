a13e28c07895d9c11ecdef849e77b192fb020626
IPHUN1989 <tamas.peter.illles@gmail.com>
2024-07-25 22:20:25 +0200

    🦄 refactor(main.go): Delete functions
    Delete unnecessary functions

--- main.go
+++ main.go
@@ -10,31 +10,13 @@ import (
 	"strconv"
 )
 
-func getHello(w http.ResponseWriter) {
+func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
 	io.WriteString(w, "Hello, World!\n")
 }
 
-func getRealHello(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./static/sites/hello.html")
-}
-
-func getError(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./static/sites/error.html")
-}
-
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/hidden", getRealHello)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" && r.URL.Path != "/hidden" {
-			w.WriteHeader(http.StatusNotFound)
-			getError(w, r)
-			return
-		}
-		getHello(w)
-	})
+	http.HandleFunc("/", getHello)
 
 	port := 3335
 	portString := strconv.Itoa(port)
