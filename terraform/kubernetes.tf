c5cc4cecd1330e4d463e1676dbf5d6a1410762c2
IPHUN1989 <tamas.peter.illles@gmail.com>
2024-07-30 16:16:04 +0200

    🐞 fix(kubernetes.tf): Add depends on
    Add depends on for deployment and service

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -4,8 +4,10 @@ resource "kubernetes_manifest" "namespace" {
 
 resource "kubernetes_manifest" "deployment" {
   manifest = yamldecode(file("../kubernetes/deployment.yaml"))
+  depends_on = [kubernetes_manifest.namespace]
 }
 
 resource "kubernetes_manifest" "service" {
   manifest = yamldecode(file("../kubernetes/service.yaml"))
+  depends_on = [kubernetes_manifest.namespace, kubernetes_manifest.deployment]
 }
